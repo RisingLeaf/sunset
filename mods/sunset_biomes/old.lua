af70228e38b53c9a8421dcd9eab35b4de74aa9db
thvk-net <control@thvk.net>
2023-01-29 19:12:31 -0800

    some stuff

--- mods/sunset_biomes/old.lua
+++ mods/sunset_biomes/old.lua
@@ -100,6 +100,20 @@ minetest.register_abm({
 		end
 	end
 })
+minetest.register_abm({
+	label = "Spawn Old Inhabitant",
+	nodenames = {"sunset_biomes:old_dirt_with_old_grass"},
+	neighbors = {},
+	interval = 10,
+	chance = 1000,
+	action = function(pos)
+		local tod = minetest.get_timeofday()
+		if tod > 0.2 and tod < 0.8 then
+			return
+		end
+		minetest.add_entity({x=pos.x, y=pos.y+1, z=pos.z}, "sunset_biomes:old_inhabitant")
+	end,
+})
 
 minetest.register_node("sunset_biomes:old_sand", {
 	description = S("Old Sand"),
--- mods/sunset_biomes/old_mobs.lua
+++ mods/sunset_biomes/old_mobs.lua
@@ -1,6 +1,7 @@
 -- Get translator:
 local S = sunset_biomes.get_translator
 
+
 minetest.register_entity("sunset_biomes:old_inhabitant", {
 	initial_properties = {
 		visual = "mesh",
@@ -8,9 +9,28 @@ minetest.register_entity("sunset_biomes:old_inhabitant", {
 		textures = {"old_inhabitant.png"},
 		backface_culling = false,
 		physical = true,
+		hp_max = 20,
+		hp = 20,
 	},
 
+	on_activate = function(self, staticdata)
+		self.walk_dir = {x=0, y=0, z=0}
+	end,
+	
 	on_step = function(self, dtime)
-		-- entity behavior code goes here
+		local tod = minetest.get_timeofday()
+		if tod > 0.2 and tod < 0.8 then
+			self.object:remove()
+			return
+		end
+
+		self.object:set_animation({x = 1, y = 60}, 24, 0)
+		if math.random() < 0.05 then
+			self.walk_dir = {x = math.random(-1, 1), y = 0, z = math.random(-1, 1), z = math.random(-0.1, 0.1)}
+		end
+		local dir = vector.normalize(self.walk_dir)
+		local yaw = math.atan(dir.z, dir.x)
+		self.object:set_yaw(yaw)
+		self.object:set_velocity(self.walk_dir)
 	end,
 })
